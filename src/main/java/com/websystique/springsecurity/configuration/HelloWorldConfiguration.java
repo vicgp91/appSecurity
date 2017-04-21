a302f271296a1e76c20046b863f5b90d59c3d242
vicgp91 <vicgp_24@hotmail.com>
2017-04-21 15:33:04 -0500

    autenticacion desde base de datos

--- src/main/java/com/websystique/springsecurity/configuration/HelloWorldConfiguration.java
+++ src/main/java/com/websystique/springsecurity/configuration/HelloWorldConfiguration.java
@@ -3,6 +3,7 @@ package com.websystique.springsecurity.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -17,44 +18,41 @@ import org.springframework.web.servlet.view.tiles3.TilesView;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.websystique.springsecurity")
 public class HelloWorldConfiguration extends WebMvcConfigurerAdapter {
-	
-	/*@Bean(name="HelloWorld")
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setViewClass(JstlView.class);
-		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setSuffix(".jsp");
-
-		return viewResolver;
-	}*/
-	
-	
-	
-	@Bean
-    public UrlBasedViewResolver viewResolver() {
-        UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
-        viewResolver.setViewClass(TilesView.class);
-        return viewResolver;
-    }
-
-    @Bean
-    public TilesConfigurer tilesConfigurer() {
-        TilesConfigurer tilesConfigurer = new TilesConfigurer();
-        tilesConfigurer.setDefinitions(new String[]{
-                "/WEB-INF/tiles.xml"
-        });
-        tilesConfigurer.setCheckRefresh(true);
-        return tilesConfigurer;
-    }
-	
-	
 
 	/*
-     * Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/theme1/");
-    }
+	 * @Bean(name="HelloWorld") public ViewResolver viewResolver() {
+	 * InternalResourceViewResolver viewResolver = new
+	 * InternalResourceViewResolver();
+	 * viewResolver.setViewClass(JstlView.class);
+	 * viewResolver.setPrefix("/WEB-INF/views/");
+	 * viewResolver.setSuffix(".jsp");
+	 * 
+	 * return viewResolver; }
+	 */
+
+	@Bean
+	public UrlBasedViewResolver viewResolver() {
+		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+		viewResolver.setViewClass(TilesView.class);
+		return viewResolver;
+	}
+
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer tilesConfigurer = new TilesConfigurer();
+		tilesConfigurer.setDefinitions(new String[] { "/WEB-INF/tiles.xml" });
+		tilesConfigurer.setCheckRefresh(true);
+		return tilesConfigurer;
+	}
+
+
+	/*
+	 * Configure ResourceHandlers to serve static resources like CSS/ Javascript
+	 * etc...
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/theme1/");
+	}
 }
--- src/main/java/com/websystique/springsecurity/configuration/SecurityConfiguration.java
+++ src/main/java/com/websystique/springsecurity/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.websystique.springsecurity.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,24 +12,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	
 	@Autowired
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("bill").password("abc123").roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password("root123").roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("dba").password("root123").roles("ADMIN","DBA");
+		/*
+		 * auth.inMemoryAuthentication().withUser("bill").password("abc123").
+		 * roles("USER");
+		 * auth.inMemoryAuthentication().withUser("admin").password("root123").
+		 * roles("ADMIN");
+		 * auth.inMemoryAuthentication().withUser("dba").password("root123").
+		 * roles("ADMIN","DBA");
+		 */
+
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/DB_CHURCHADMINISTRATOR");
+		driverManagerDataSource.setUsername("root");
+		driverManagerDataSource.setPassword("12345678");
+		auth.jdbcAuthentication().dataSource(driverManagerDataSource)
+				.usersByUsernameQuery("select username,password, enabled from users where username=?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	  
-	  http.authorizeRequests()
-	  	.antMatchers("/", "/home").permitAll()
-	  	.antMatchers("/admin/**").access("hasRole('ADMIN')")
-	  	.antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
-	  	.and().formLogin().loginPage("/login")
-	  	.usernameParameter("ssoId").passwordParameter("password")
-	  	.and().csrf()
-	  	.and().exceptionHandling().accessDeniedPage("/Access_Denied");
+
+		http.authorizeRequests().antMatchers("/", "/home").permitAll().antMatchers("/admin/**")
+				.access("hasRole('ADMIN')").antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')").and()
+				.formLogin().loginPage("/login").usernameParameter("ssoId").passwordParameter("password").and().csrf()
+				.and().exceptionHandling().accessDeniedPage("/Access_Denied");
 	}
 }
